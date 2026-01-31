1e6a8586508786b961bb25f9d958d41f07efefa6
dsrisid <srikanth.devanaboina@gmail.com>
2026-01-31 19:15:05 +0530

    Update app.py

--- superkart_assignment/deployment/app.py
+++ superkart_assignment/deployment/app.py
@@ -4,7 +4,7 @@ from huggingface_hub import hf_hub_download
 import joblib
 
 # Download and load the trained model
-model_path = hf_hub_download(repo_id="dsrisid/super-kart-sales-prediction/superkart_revenue_model", filename="best_superkart_revenue_model_v1.joblib")
+model_path = hf_hub_download(repo_id="dsrisid/super-kart-sales-prediction", filename="best_superkart_revenue_model_v1.joblib")
 model = joblib.load(model_path)
 
 # Streamlit UI
